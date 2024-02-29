77b1953c74f2d04e1a62736652efee1f6e0c8fb3
etachott <bfwaend@gmail.com>
2024-02-29 19:40:47 -0300

    Add TodoItem component

---
+++ components/TodoItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { TodoItem } from '@/interfaces/Todo'
+
+defineProps<{
+  todo: TodoItem,
+}>()
+</script>
+
+<template>
+  <v-list-item :value="todo.id">
+    <template v-slot:prepend="{ isActive }">
+      <v-list-item-action start>
+        <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+      </v-list-item-action>
+    </template>
+    <div class="d-flex align-center justify-space-between">
+      <v-list-item-title class="text-body-1">{{todo.text}}</v-list-item-title>
+      <v-btn @click="$emit('delete', todo.id)">Delete</v-btn>
+    </div>
+  </v-list-item>
+</template>
