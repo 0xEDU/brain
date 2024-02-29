efaf3ecfd152919fa392cb891580d30842c981dd
etachott <bfwaend@gmail.com>
2024-02-29 19:41:19 -0300

    Add logic for todo list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+import TodoItem from '~/components/TodoItem.vue';
+import type { Todo } from '~/interfaces/Todo';
+
+const todos = ref<Todo[]>([])
+const newTodo = ref('')
+
+function addTodo() {
+  todos.value.push({
+    id: Date.now(),
+    text: newTodo.value,
+    completed: false
+  })
+  newTodo.value = ''
+}
+
+function deleteTodo(id: number) {
+  todos.value = todos.value.filter((todo) => todo.id !== id)
+}
+
+</script>
 <template>
-  <div>
-    🧠 brainzz
-  </div>
+  <v-container class="align-center">
+    <v-row align="center">
+      <v-col align="center">
+        <h1>My Brainzzzz</h1>
+        <v-text-field v-model="newTodo" @keyup.enter="addTodo"></v-text-field>
+        <v-list>
+          <TodoItem
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+            @delete="deleteTodo"
+          />
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
